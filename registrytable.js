671b901f6ffb6e75268599b7cdf7061b11678296
Bill Xiong <wwx@duke.edu>
2018-04-27 23:39:33 -0400

    implemented basic registry table

--- registrytable.js
+++ registrytable.js
@@ -2,7 +2,6 @@ var redis = require("redis");
 var bluebird = require("bluebird");
 
 
-
 class RegistryTable {
 
     constructor(redis_host, redis_port) {
@@ -11,15 +10,19 @@ class RegistryTable {
     }
 
     registerDiscoveryService(name, host, port) {
-    
+        //returns promise to client
+        return this._redisClient.setAsync(name, this._concatValue(host, port));
     }
 
     retrieveDiscoveryService(name) {
-    
+
+        return this._redisClient.getAsync(name);
     }
 
+    _concatValue(host, port) {
+        return host + ":" + port;
+    }
 
 }
-
 
 module.exports = RegistryTable;
